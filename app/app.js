2fb95dd785e7013f35f33c3f566f602dd9c90a57
Sebastian Geiger <sbastig@gmx.net>
2015-11-16 14:13:52 +0100

    Teachers: Added three tabs
      * Profile: Tab with widgets about teacher and his
                 students
      * Studentprogress: TODO
      * Progress Dashboard: TODO

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
     'ui.router',
+    'ui.bootstrap',
     'common.exercise',
     'myApp.profiles',
     'myApp.profiles.students',
--- app/profiles/teachers/teachers.js
+++ app/profiles/teachers/teachers.js
@@ -3,6 +3,16 @@ angular.module('myApp.profiles.teachers', [])
 .config (['$stateProvider', function ($stateProvider) {
     $stateProvider.state ('profiles.teachers', {
         url: '/teachers',
-        templateUrl: 'profiles/teachers/teachers.html'
+        templateUrl: 'profiles/teachers/teachers.html',
+        controller: 'TeachersCtrl'
     })
-}]);
+}])
+
+.controller('TeachersCtrl', [ '$scope',
+    function ($scope) {
+        $scope.tabs =  [
+            { title:'Dynamic Title 1', content:'Dynamic content 1' },
+            { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
+        ];
+    }
+]);
