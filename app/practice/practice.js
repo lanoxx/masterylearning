c42e6eef1efd47181e9a9ad7075753c51565cd6d
Sebastian Geiger <sbastig@gmx.net>
2015-12-03 17:39:45 +0100

    First try at the modal dialog.

--- app/practice/practice.js
+++ app/practice/practice.js
@@ -25,10 +25,52 @@ angular.module ('myApp.practice', ['ui.router'])
 
             .state('practice.formalmethods.propositionallogic.unit1', {
                 url: '/unit1',
-                templateUrl: 'practice/unit1.html'
+                templateUrl: 'practice/unit1.html',
+                controller: 'Unit1Ctrl'
             })
             .state('practice.manage', {
                 url: '/manage',
                 templateUrl: 'practice/manage.html'
             });
-    }]);
+    }])
+
+    .controller('Unit1Ctrl', ['$scope', '$uibModal', function ($scope, $uibModal) {
+        $scope.items = ['item1', 'item2', 'item3'];
+
+        $scope.open = function () {
+
+            var modalInstance = $uibModal.open({
+                animation: true,
+                templateUrl: 'practice/unit1modal.html',
+                controller: 'ModalInstanceCtrl',
+                size: 'lg',
+                resolve: {
+                    items: function () {
+                        return $scope.items;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (selectedItem) {
+                $scope.selected = selectedItem;
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
+    }])
+
+    .controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items) {
+
+        $scope.items = items;
+        $scope.selected = {
+            item: $scope.items[0]
+        };
+
+        $scope.ok = function () {
+            $uibModalInstance.close($scope.selected.item);
+        };
+
+        $scope.cancel = function () {
+            $uibModalInstance.dismiss('cancel');
+        };
+});
