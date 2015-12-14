8c01882f670122f7535d868ff825f272e16d90bd
Sebastian Geiger <sbastig@gmx.net>
2015-12-14 15:46:48 +0100

    mathmode: Cleanup

--- app/components/mathmode/mathmode.js
+++ app/components/mathmode/mathmode.js
@@ -8,15 +8,7 @@ angular.module('common.mathmode', [])
         return {
             restrict: "A",
             controller: ["$scope", "$element", "$attrs",
-                function($scope, $element, $attrs) {
-                    console.log($element);
-                    /*$scope.$watch($attrs.mathjaxBind, function(texExpression) {
-                     var texScript = angular.element("<script type='math/tex'>")
-                     .html(texExpression ? texExpression :  "");
-                     $element.html("");
-                     $element.append(texScript);
-                     MathJax.Hub.Queue(["Reprocess", MathJax.Hub, $element[0]]);
-                     });*/
+                function($scope, $element) {
                     MathJax.Hub.Queue(["Typeset", MathJax.Hub, $element[0]]);
                 }]
         };
