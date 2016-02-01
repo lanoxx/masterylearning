1d97057b8b0769f1bae749b615cb9ffe1aaf2a1b
Sebastian Geiger <sbastig@gmx.net>
2016-02-01 18:38:21 +0100

    Remove remaining mathjax references

--- app/student/courses/entries/entries.js
+++ app/student/courses/entries/entries.js
@@ -138,24 +138,6 @@ angular.module('myapp.student.courses.entries', ['ui.router'])
         })
     }])
 
-    .directive("mathjaxBind", function ()
-    {
-        return {
-            restrict: "A",
-            controller: ["$scope", "$element", "$attrs", function ($scope, $element, $attrs)
-            {
-                $scope.$watch($attrs.mathjaxBind, function (value)
-                {
-                    var $script = angular.element("<script type='math/tex'>")
-                        .html(value == undefined ? "" : value);
-                    $element.html("");
-                    $element.append($script);
-                    MathJax.Hub.Queue(["Reprocess", MathJax.Hub, $element[0]]);
-                });
-            }]
-        };
-    })
-
     .controller ('EntriesCtrl', ['$scope', 'entry', '$log', function ($scope, entry, $log)
     {
         $log.info ('[myApp] EntriesCtrl running');
