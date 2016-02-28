d90e9cc7d699d94c8a3042ff3614867f5ef7a22c
Sebastian Geiger <sbastig@gmx.net>
2016-02-28 13:55:48 +0100

    Course: add get_entry and get_entries methods

--- app/services/database/course.js
+++ app/services/database/course.js
@@ -33,6 +33,66 @@ angular.module ('myapp.factories.course', ['myapp.factories.entry'])
             return entry;
         };
 
+        Course.prototype.get_entry = function (id)
+        {
+            var entry;
+            for (var i = 0, n = this.entries.length; i < n; i++)
+            {
+                entry = search (this.entries[i], id);
+                if (entry !== null)
+                    break;
+            }
+
+            return entry;
+
+            function search (entry, id)
+            {
+                var candidate = null;
+
+                if (entry.id === id)
+                    candidate = entry;
+                else {
+                    for (var i = 0, n = entry.children.length; i < n; i++) {
+                        candidate = search (entry.children[i], id);
+                        if (candidate !== null)
+                            break;
+                    }
+                }
+
+                return candidate;
+            }
+        };
+
+        Course.prototype.get_entries = function (type)
+        {
+            var entries = [];
+
+            entries.push.apply (entries, collect(this.entries, type));
+
+            function collect(entries, type)
+            {
+                var result = [];
+
+                if (!entries || entries.length === 0)
+                    return [];
+
+                entries.forEach(function (entry)
+                {
+                    if (!type)
+                        result.push(entry);
+                    else if (type && entry.data.type === type)
+                        result.push(entry);
+
+                    if (entry.hasOwnProperty("children")) {
+                        result.push.apply(result, collect(entry.children, type));
+                    }
+                });
+                return result;
+            }
+
+            return entries;
+        };
+
         Course.prototype.print = function ()
         {
             var result = "[myApp] Course (id=" + this.id + ", title=" + this.title + ", period=" + this.period + ", description=" + this.description + ",\n";
