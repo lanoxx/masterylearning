d0af0dab7702630c1ec33df4cd3d95435541cfc3
Sebastian Geiger <sbastig@gmx.net>
2016-03-01 11:12:56 +0100

    services/contentservice: Add a new content service
    This service is used to enumerate entries of the content tree

---
+++ app/services/contentservice.js
@@ -0,0 +1,97 @@
+angular.module ('myapp.services.content', [])
+
+    .factory ('ContentService', ['$log', function ($log)
+    {
+        function ContentService (root, blocking_strategy, filter_strategy)
+        {
+            this.entry_stack = [root];
+            this.blocking_strategy = blocking_strategy;
+            this.filter_strategy = filter_strategy;
+            this.exercise_tree = false;
+        }
+
+        ContentService.prototype.push = function (root)
+        {
+            this.entry_stack.push (root);
+        };
+
+        ContentService.prototype.set_exercise_tree = function ()
+        {
+            this.exercise_tree = true;
+        };
+
+        ContentService.prototype.enumerate_tree = function ()
+        {
+            var entries = [];
+            var current_entry;
+
+            if (include_root)
+                entries.push(root);
+
+            current_entry = root;
+
+            while ((current_entry = current_entry.next ()))
+            {
+                entries.push (current_entry);
+
+                if (blocking_strategy (current_entry))
+                {
+                    return entries;
+                }
+            }
+
+            return entries;
+        };
+
+        ContentService.prototype.enumerate_subtree = function ()
+        {
+            var entries = [];
+            var push = function push (current_entry) {
+                if (this.filter_strategy (current_entry) || this.exercise_tree)
+                    entries.push (current_entry);
+            }.bind (this);
+
+            var current_entry;
+
+            current_entry = this.entry_stack.pop();
+            push (current_entry);
+
+            if (current_entry.has_children())
+            {
+                current_entry = current_entry.children[0];
+            } else {
+                if (current_entry.has_next_sibling())
+                    this.entry_stack.push (current_entry.next_sibling ());
+                return entries;
+            }
+
+            while (current_entry || this.entry_stack.length > 0)
+            {
+                push (current_entry);
+
+                if (this.blocking_strategy (current_entry))
+                {
+                    if (current_entry.has_next_sibling())
+                        this.entry_stack.push(current_entry.next_sibling());
+                    return entries;
+                }
+
+                if (current_entry.has_children())
+                {
+                    if (current_entry.has_next_sibling())
+                        this.entry_stack.push (current_entry.next_sibling());
+                    current_entry = current_entry.children[0];
+                } else {
+                    current_entry = current_entry.next_sibling();
+                    if (!current_entry) {
+                        current_entry = this.entry_stack.pop ();
+                        this.exercise_tree = false;
+                    }
+                }
+            }
+
+            return entries;
+        };
+
+        return ContentService;
+    }]);
