7e5076a54768c76cf88708f0911efb431e503e05
Sebastian Geiger <sbastig@gmx.net>
2016-03-02 14:15:15 +0100

    student-home: move controller and config to student-home.js and render active_courses of User

--- app/student/student-home.js
+++ app/student/student-home.js
@@ -1,1 +1,35 @@
-angular.module ('myApp.student', ['ui.router', 'ngSanitize']);
+angular.module ('myApp.student', ['ui.router', 'ngSanitize'])
+
+    .config (['$stateProvider', function ($stateProvider)
+    {
+        $stateProvider.state ('home.student', {
+            url: '/student',
+            views: {
+                'navigation@': {
+                    templateUrl: 'navigation.html',
+                    controller: 'NavigationController'
+                },
+                '@': {
+                    templateUrl: 'student/student-home.html',
+                    controller: 'StudentController'
+                }
+            },
+            role: 'ROLE_STUDENT'
+        })
+    }])
+
+    .controller ('StudentController', ['$scope', 'UserService', 'CourseHistory', 'database', '$log', function ($scope, UserService, CourseHistory, database, $log)
+    {
+        $log.info ("[myApp] StudentController running");
+
+        $scope.active_courses = UserService.active_courses;
+
+        $scope.get_course_title = function (course_id)
+        {
+            var course;
+
+            course = database.get_course (course_id);
+
+            return course.title + " " + course.period;
+        };
+    }]);
