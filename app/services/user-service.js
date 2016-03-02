8ba0b250b14b72f6e78242ffc4656a5dff8f5e3a
Sebastian Geiger <sbastig@gmx.net>
2016-03-02 14:12:17 +0100

    user-service: add CourseHistory and EntryHistory

--- app/services/user-service.js
+++ app/services/user-service.js
@@ -1,7 +1,67 @@
 angular.module ('myapp.services.user', [])
 
-    .service ('UserService', [function ()
+    .service ('UserService', [function UserService()
     {
         this.currentUser = null;
         this.role = "ROLE_GUEST";
-    }]);
+
+        /**
+         *
+         * @type {Array.<CourseHistory>}
+         */
+        this.active_courses = [];
+    }])
+
+    .factory ('CourseHistory', ['EntryHistory', function CourseHistoryFactory(EntryHistory)
+    {
+        /**
+         * @param course_id:String
+         * @constructor
+         */
+        function CourseHistory (course_id) {
+            this.course_id = course_id;
+            this.last_entry_id = null;
+            this.visited_entries = [];
+        }
+
+        CourseHistory.prototype.set_last_entry = function (entry)
+        {
+            this.last_entry_id = entry.id;
+        };
+
+        CourseHistory.prototype.add_visited_entry = function (entry, state)
+        {
+            var visited_entry = new EntryHistory(entry, state);
+
+            this.visited_entries.push (visited_entry);
+
+            return visited_entry;
+        };
+
+        return CourseHistory;
+    }])
+
+
+    .factory ('EntryHistory', function EntryHistoryFactory ()
+    {
+        function EntryHistory (entry, state)
+        {
+            this.course_id = null;
+            this.entry_id = entry.id;
+
+            if (state !== undefined)
+                this.state = state;
+            else
+                this.state = null;
+        }
+
+        /**
+         * @param state:String The current state of the EntryData object.
+         */
+        EntryHistory.prototype.update_state = function (state)
+        {
+            this.state = state;
+        };
+
+        return EntryHistory;
+    });
