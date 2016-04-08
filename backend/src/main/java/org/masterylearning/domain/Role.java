21be6bfbc6d83815ba153fc0327ba49eaa5ecdc8
Sebastian Geiger <sbastig@gmx.net>
2016-04-08 23:42:40 +0200

    Role: Add persistence annotations

--- backend/src/main/java/org/masterylearning/domain/Role.java
+++ backend/src/main/java/org/masterylearning/domain/Role.java
@@ -1,9 +1,16 @@
 package org.masterylearning.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * Created by sebastiangeiger on 8/03/16.
  */
+@Entity
 public class Role {
+    @Id
+    @GeneratedValue
     public String id;
     public String role;
     public String description;
