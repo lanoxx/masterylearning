322b119a98dd465cc6e1b0c0323e90e982b30dc0
Sebastian Geiger <sbastig@gmx.net>
2016-04-11 16:00:36 +0200

    InitializationController: create user with encoded password and three roles

--- backend/src/main/java/org/masterylearning/web/InitializationController.java
+++ backend/src/main/java/org/masterylearning/web/InitializationController.java
@@ -1,12 +1,15 @@
 package org.masterylearning.web;
 
+import org.masterylearning.domain.Role;
 import org.masterylearning.domain.User;
 import org.masterylearning.repository.UserRepository;
+import org.masterylearning.service.UserService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import java.util.List;
 
 /**
@@ -16,15 +19,32 @@ import java.util.List;
 public class InitializationController {
 
     @Inject UserRepository userRepository;
+    @Inject UserService userService;
 
     @RequestMapping (method = RequestMethod.GET, path = "/users")
+    @Transactional
     public Boolean initUsers () {
 
         List<User> all = userRepository.findAll ();
 
         if (all.size () == 0) {
 
-            User user = new User ("user", "123456");
+            User user = userService.createUser ("user", "123456");
+
+            Role role = new Role ();
+            role.name = "STUDENT";
+            role.description = "";
+            user.getRoles ().add (role);
+
+            role = new Role ();
+            role.name = "TEACHER";
+            role.description = "";
+            user.getRoles ().add (role);
+
+            role = new Role ();
+            role.name = "ADMIN";
+            role.description = "";
+            user.getRoles ().add (role);
 
             userRepository.save (user);
 
