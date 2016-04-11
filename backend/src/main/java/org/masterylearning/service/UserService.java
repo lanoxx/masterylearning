3345ed35ae90e86e51dfcbab23e3b1fc2a64e015
Sebastian Geiger <sbastig@gmx.net>
2016-04-11 15:57:32 +0200

    UserService: Add method to create user
    The given password will be encoded by a PasswordEncoder

--- backend/src/main/java/org/masterylearning/service/UserService.java
+++ backend/src/main/java/org/masterylearning/service/UserService.java
@@ -1,9 +1,26 @@
 package org.masterylearning.service;
 
+import org.masterylearning.domain.User;
+import org.masterylearning.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
 
 @Service
 public class UserService {
 
+    @Inject PasswordEncoder passwordEncoder;
+    @Inject UserRepository userRepository;
 
+    public User createUser (String username, String password) {
+
+        String encodedPassword = passwordEncoder.encode (password);
+
+        User user = new User (username, encodedPassword);
+
+        userRepository.save (user);
+
+        return user;
+    }
 }
