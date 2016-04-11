27cd1da4d16d9de64167f544abd210338bea4305
Sebastian Geiger <sbastig@gmx.net>
2016-04-11 15:36:57 +0200

    RoleRepository: Add new class

---
+++ backend/src/main/java/org/masterylearning/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package org.masterylearning.repository;
+
+import org.masterylearning.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ */
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
