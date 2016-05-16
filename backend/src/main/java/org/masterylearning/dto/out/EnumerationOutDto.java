64358747f0717cee1a89b7389a9f5f29804600f5
Sebastian Geiger <sbastig@gmx.net>
2016-05-16 23:55:13 +0200

    [Backend] HistoryController: Implement entry enumeration

--- backend/src/main/java/org/masterylearning/dto/out/EnumerationOutDto.java
+++ backend/src/main/java/org/masterylearning/dto/out/EnumerationOutDto.java
@@ -7,4 +7,5 @@ import java.util.List;
 public class EnumerationOutDto {
     public List<EntryDataOutDto> entries;
     public Long nextId;
+    public Long scrollLocation; // location to which the browser should scroll after all entries have been rendered.
 }
--- backend/src/main/java/org/masterylearning/web/HistoryController.java
+++ backend/src/main/java/org/masterylearning/web/HistoryController.java
@@ -4,12 +4,21 @@ import org.masterylearning.domain.Course;
 import org.masterylearning.domain.CourseHistory;
 import org.masterylearning.domain.Entry;
 import org.masterylearning.domain.EntryHistory;
+import org.masterylearning.domain.User;
+import org.masterylearning.domain.data.ContinueButton;
+import org.masterylearning.domain.data.EntryData;
+import org.masterylearning.domain.data.Exercise;
 import org.masterylearning.dto.out.CourseHistoryOutDto;
 import org.masterylearning.dto.out.CourseOutDto;
 import org.masterylearning.dto.out.EntryDataOutDto;
+import org.masterylearning.dto.out.EnumerationOutDto;
 import org.masterylearning.repository.CourseRepository;
+import org.masterylearning.repository.EntryHistoryRepository;
+import org.masterylearning.service.BlockingStrategy;
 import org.masterylearning.service.CourseService;
 import org.masterylearning.service.HistoryService;
+import org.masterylearning.service.TreeEnumerator;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -30,6 +40,7 @@ import java.util.stream.Collectors;
 public class HistoryController {
 
     @Inject CourseRepository courseRepository;
+    @Inject EntryHistoryRepository entryHistoryRepository;
     @Inject CourseService courseService;
     @Inject HistoryService historyService;
 
@@ -103,5 +114,118 @@ public class HistoryController {
         }
 
         return result;
+    }
+
+    @CrossOrigin
+    @RequestMapping(path = "/courses/{courseId}/enumerate/{entryId}", method = RequestMethod.GET)
+    @Transactional
+    public EnumerationOutDto enumerateEntries (@PathVariable Long courseId, @PathVariable Long entryId) {
+
+        Object principal = SecurityContextHolder.getContext ()
+                                                .getAuthentication ()
+                                                .getPrincipal ();
+
+        if (!(principal instanceof User)) {
+            return null;
+        }
+
+        User user = (User) principal;
+
+        Entry root;
+        CourseHistory courseHistory;
+        List<EntryHistory> entryHistoryList;
+        EnumerationOutDto dto = new EnumerationOutDto ();
+
+        Course course = courseRepository.findOne (courseId);
+
+        if (course == null) {
+            //TODO return error
+            return null;
+        }
+
+        root = courseService.find (course, entryId);
+        courseHistory = historyService.getCourseHistory (courseId);
+
+        entryHistoryList = courseHistory.getEntryHistoryList ();
+
+        // this blocking strategy blocks on ContinueButtons and Exercises but only
+        // if the user has not previously seen the entry, that is the entry is not
+        // in his history yet
+        BlockingStrategy blockingStrategy = entry -> {
+            boolean blockingCandidate = entry.data instanceof ContinueButton || entry.data instanceof Exercise;
+            if (blockingCandidate) {
+                return entryHistoryList.stream ()
+                                       .filter (entryHistory -> entryHistory.entry.id.equals (entry.id))
+                                       .map (entryHistory -> {
+                                           if (entry.data instanceof ContinueButton) {
+                                               // the entry is already in the users history: dont block
+                                               return false;
+                                           }
+                                           if (entry.data instanceof Exercise) {
+                                               // we must check if the exercise has been answered, only then we block!
+                                               if (entryHistory.state != null) {
+                                                   if (entryHistory.state.contains ("answered")) {
+                                                       return true;
+                                                   }
+                                               }
+                                               return false;
+                                           }
+                                           return false;
+                                       })
+                                       .reduce (true, (a, b) -> false);
+            }
+            return false;
+        };
+        TreeEnumerator treeEnumerator = new TreeEnumerator (root, blockingStrategy);
+
+        List<EntryData> entryDatas = treeEnumerator.enumerateTree ();
+
+        dto.entries = entryDatas.stream ()
+                                .map (entry -> {
+                                    Optional<EntryHistory> entryHistory = getEntryHistory (entryHistoryList, entry.container);
+                                    EntryDataOutDto outDto = entry.toDto ();
+
+                                    if (!entryHistory.isPresent ()) {
+                                        addEntryToUserHistory (entryHistoryList, courseHistory, course, entry.container);
+                                    } else {
+                                        outDto.state = entryHistory.get ().state;
+                                        outDto.seen = true;
+                                    }
+                                    return outDto;
+                                })
+                                .filter (outDto -> !(outDto.type.equals ("continue-button") && outDto.seen))
+                                .collect(Collectors.toList());
+
+        List<Long> locations = dto.entries.stream ()
+                                .filter (entryOut -> entryOut.seen)
+                                .map (entryOut -> entryOut.id)
+                                .collect (Collectors.toList ());
+
+
+        dto.scrollLocation = locations.size () > 0
+                             ? locations.get (locations.size () - 1)
+                             : null;
+
+        if (treeEnumerator.entryStack.size () > 0) {
+            Entry next = treeEnumerator.entryStack.peek ();
+            dto.nextId = next.id;
+        }
+
+        return dto;
+    }
+
+    private Optional<EntryHistory> getEntryHistory (List<EntryHistory> entryHistoryList, Entry entry) {
+        return entryHistoryList.stream ()
+                               .filter (entryHistory -> entryHistory.entry.id.equals (entry.id))
+                               .findFirst ();
+    }
+
+    private void addEntryToUserHistory (List<EntryHistory> entryHistoryList, CourseHistory courseHistory, Course course, Entry entry) {
+        EntryHistory entryHistory = new EntryHistory ();
+        entryHistory.courseHistory = courseHistory;
+        entryHistory.entry = entry;
+        entryHistory.course = course;
+        entryHistoryList.add (entryHistory);
+        entryHistoryRepository.save (entryHistoryList);
     }
 }
