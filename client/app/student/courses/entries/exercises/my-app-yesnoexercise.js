64826ae3132c73edd6cfd318ef7f37b191415d50
Sebastian Geiger <sbastig@gmx.net>
2016-05-24 12:49:44 +0200

    [Client] YesNoExercise: Load and store state

--- client/app/student/courses/entries/exercises/my-app-yesnoexercise.js
+++ client/app/student/courses/entries/exercises/my-app-yesnoexercise.js
@@ -7,7 +7,7 @@ angular.module ('myapp.student.courses.entries.exercises')
      *            { title:String, text:String, answer:boolean }
      * @onanswered: A callback to be called by the directive when the user has selected an answer.
      */
-    .directive ('myAppYesNoExercise', ['$log', '$sanitize', function ($log, $sanitize)
+    .directive ('myAppYesNoExercise', ['$log', '$sanitize', 'HistoryService', function ($log, $sanitize, HistoryService)
     {
         return {
             scope: {
@@ -20,29 +20,57 @@ angular.module ('myapp.student.courses.entries.exercises')
                 $scope.answer = null;
                 $scope.answered = false;
 
-                $scope.sanitize = function (text)
-                {
-                    return $sanitize (text);
-                };
+                $scope.sanitize = sanitize;
+                $scope.check_cb = check_cb;
+                $scope.is_answer_correct = is_answer_correct;
+                $scope.is_answer_wrong = is_answer_wrong;
 
-                $scope.check_cb = function ()
+                init();
+
+                function init ()
                 {
+                    if (!$scope.exercise.state) {
+                        return;
+                    }
+
+                    var stateTokens = $scope.exercise.state.split (";");
+
+                    $scope.answer = stateTokens[1] !== "false";
+
+                    check_answer();
+                }
+
+                function is_answer_correct ()
+                {
+                    return $scope.answered && $scope.answer === $scope.exercise.answer;
+                }
+
+                function is_answer_wrong ()
+                {
+                    return $scope.answered && $scope.answer !== $scope.exercise.answer;
+                }
+
+                function check_answer () {
+                    "use strict";
+
                     $scope.answered = true;
 
                     $scope.correct = $scope.answer === $scope.exercise.answer;
+                }
+
+                function check_cb ()
+                {
+                    check_answer();
+
+                    HistoryService.setEntryState().save ({ courseId: $scope.exercise.courseId, entryId: $scope.exercise.id }, { state: $scope.correct + ";" + $scope.answer + "" });
 
                     $scope.onanswered ({answer_model: $scope.answer, answer: $scope.correct});
-                };
+                }
 
-                $scope.is_answer_correct = function ()
+                function sanitize (text)
                 {
-                    return $scope.answered && $scope.answer === $scope.exercise.answer;
-                };
-
-                $scope.is_answer_wrong = function ()
-                {
-                    return $scope.answered && $scope.answer !== $scope.exercise.answer;
-                };
+                    return $sanitize (text);
+                }
             }],
             templateUrl: 'student/courses/entries/exercises/my-app-yesnoexercise.tpl.html'
         };
